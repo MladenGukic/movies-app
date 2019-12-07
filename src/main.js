b99b5e941dd81c214e439bb3b6258ad0c46c385d
MladenGukic <mladengukic@gmail.com>
2019-12-07 20:45:28 +0100

    Zadatak 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import AppMovies from './components/AppMovies'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  {name: 'index', path: '/', redirect: 'movies'},
+  {name: 'movies', path: '/movies', component: AppMovies}
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
