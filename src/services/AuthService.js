fe461cfdcdfc9d641842b1f46f6a740b4bb7dee1
MladenGukic <mladengukic@gmail.com>
2019-12-11 17:09:40 +0100

    small changes

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,10 +3,20 @@ import axios from 'axios';
 export default class AuthService {
     constructor() {
         axios.defaults.baseURL = 'http://localhost:8000/api/'
+
+        axios.defaults.headers.common = {
+            'Authorization':`Bearer ${localStorage.getItem('token')}`,
+             accept: 'application/json'
+        }
+
     }
 
     login(credentials) {
         return axios.post("login", credentials)
+        .then((r) => {
+            axios.defaults.headers.common.Authorization = `Bearer ${r.data.token}`
+            return r
+        })
     }
 
     register(user) {
